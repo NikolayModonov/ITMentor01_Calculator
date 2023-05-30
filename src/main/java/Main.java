4c6ad34b78a85d1a79eb6b1fc246e28366aeab6a
NikolayModonov <nregspam@gmail.com>
2023-05-31 03:22:15 +0500

    First commit

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,48 +1,33 @@
 package main.java;
 
 import java.util.Scanner;
+import java.io.Console;
 
 public class Main {
     public static void main(String[] args) {
         String messageInfo = "This is Calculator App\n" +
                 "Calculator works with integers from 1 to 10\n" +
                 "Calculator can do 4 operations: + - * /\n" +
-                "Example of input: 6 + 5\n" +
-                "Write 'info' for this message or 'exit' for exit";
+                "Example of input: 6 + 5";
         String input;
 
-        boolean exit = false;
-        Scanner scanner;
-
         System.out.println(messageInfo);
-        while (exit != true) {
-            scanner = new Scanner(System.in);
-            input = scanner.nextLine();
-            input = input.trim();
-            switch (input) {
-                case "exit":
-                    exit = true;
-                    break;
-                case "info":
-                    System.out.println(messageInfo);
-                    break;
-                default:
-                    System.out.println(calc(input));
-            }
-            System.out.println();
-        }
+        Scanner scanner = new Scanner(System.in);
+        System.out.println(calc(scanner.nextLine()));
+        System.out.println("Press Enter to exit");
+        scanner.nextLine();
     }
 
     public static String calc(String input){
-        String messageWrongInput = "Your input is wrong.";
-        String messageWrongOperands = "Your operands is wrong.";
-        String messageWrongOperation = "Your operation is wrong.";
+        String messageWrongInput = "Input string is wrong.";
+        String messageWrongOperands = "Operands is wrong.";
+        String messageWrongOperation = "Operation is wrong.";
         String messageWrongDivisionByZero = "Division by zero is unacceptable.";
 
         String[] tokens = input.split(" ");
 
         if (tokens.length != 3) {
-            return messageWrongInput;
+            throw new IllegalArgumentException(messageWrongInput);
         }
 
         int operand1;
@@ -51,13 +36,13 @@ public class Main {
             operand1 = Integer.parseInt(tokens[0]);
             operand2 = Integer.parseInt(tokens[2]);
         } catch (NumberFormatException e) {
-            return messageWrongOperands;
+            throw new IllegalArgumentException(messageWrongOperands);
         }
         if ((operand1 < 1) || (operand1 > 10) || (operand2 < 1) || (operand2 > 10)) {
-            return messageWrongOperands;
+            throw new IllegalArgumentException(messageWrongOperands);
         }
 
-        double result;
+        int result;
         switch (tokens[1]) {
             case "+":
                 result = operand1 + operand2;
@@ -70,12 +55,12 @@ public class Main {
                 break;
             case "/":
                 if (operand2 == 0) {
-                    return messageWrongDivisionByZero;
+                    throw new IllegalArgumentException(messageWrongDivisionByZero);
                 }
                 result = operand1 / operand2;
                 break;
             default:
-                return messageWrongOperation;
+                throw new IllegalArgumentException(messageWrongOperation);
         }
 
         return String.valueOf(result);
